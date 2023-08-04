110fdd7fdd531a0a37440c2b9b4b5ec8d65ad0e6
toluno <unot46@gmail.com>
2023-08-04 15:33:09 -0400

    Fully FPS independent

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ var canvasArea = { //Canvas Object
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         
         prevDateNow = Date.now(); // For Delta Time
-        this.interval = setInterval(updateGameArea, 0);
+        this.interval = setInterval(updateGameArea, 10); // Number sets the taget frame rate. 1000/# = FPS
     },
 
     clear : function() { // CLEARS WHOLE CANVAS
@@ -92,32 +92,25 @@ class Player {
     redraw(dt) { // DT just getting passed for debug text
 
         const ctx = canvasArea.ctx;
-        ctx.lineWidth = 5;
-        ctx.strokeStyle = "#ff0000"
+        // ctx.lineWidth = 5;
+        // ctx.strokeStyle = "#ff0000"
         
         ctx.save(); // Saves the state of the canvas
         
         ctx.translate(this.x, this.y);
 
- 
+        // DRAWING SHADOW
+        ctx.beginPath();
         ctx.ellipse(0, 0, 16, 12, 0, 0, 2 * Math.PI);
+        ctx.closePath();
         ctx.fillStyle = "#00000040";
         ctx.fill();
 
-        ctx.beginPath();
 
-        ctx.fillStyle = "#ff0000";
-
-        ctx.translate(0, -this.jumpValue);
-        ctx.rotate(this.angle * Math.PI/180);
-        ctx.fillRect(-16, -16, 32, 32); // Bottom
-
-        ctx.rotate(-this.angle * Math.PI/180); // Straiten out canvas
-        ctx.translate(0, -32);
-        ctx.rotate(this.angle * Math.PI/180); // Rerotating canvas
-
+        // DRAWING PLAYER TOP
+        ctx.translate(0, -this.jumpValue - 32); 
+        ctx.rotate(this.angle * Math.PI/180); // rotating canvas
         ctx.drawImage(document.getElementById("playerTop"), -16, -16);
-        // ctx.fillRect(-16, -16, 32, 32); // Top
 
         ctx.restore();
 
@@ -173,37 +166,6 @@ class Player {
             ctx.fill();
         }
 
-
-        /*
-        // DEBUG CORNER MARKERS
-        ctx.fillStyle = "#0000FF";
-
-        ctx.fillRect( // BOT BOT LEFT
-            this.x - (16 * Math.cos(angleRad) + (16 * Math.sin(angleRad)))
-            , 
-            this.y -this.jumpValue - (16 * Math.sin(angleRad) - (16 * Math.cos(angleRad)))
-            , 5, 5)
-
-        ctx.fillRect( // BOT BOT RIGHT
-            this.x + (16 * Math.cos(angleRad) - (16 * Math.sin(angleRad)))
-            , 
-            this.y - this.jumpValue + (16 * Math.sin(angleRad) + (16 * Math.cos(angleRad)))
-            , 5, 5)
-
-        ctx.fillRect( // BOT TOP LEFT
-            this.x - (16 * Math.cos(angleRad) - (16 * Math.sin(angleRad)))
-            , 
-            this.y - this.jumpValue - (16 * Math.sin(angleRad) + (16 * Math.cos(angleRad)))
-            , 5, 5)
-
-        ctx.fillRect( // BOT TOP RIGHT
-            this.x + (16 * Math.cos(angleRad) + (16 * Math.sin(angleRad)))
-            , 
-            this.y - this.jumpValue + (16 * Math.sin(angleRad) - (16 * Math.cos(angleRad)))
-            , 5, 5)
-        */
-
-
         ctx.restore();
 
 
@@ -212,28 +174,32 @@ class Player {
         canvasArea.ctx.fillText("angle: " + this.angle, 300, 40);
         canvasArea.ctx.fillText("dragAmount: " + touchHandler.dragAmount, 300, 60);
         canvasArea.ctx.fillText("fps: " + Math.round(100/dt), 300, 80);
+        canvasArea.ctx.fillText("DT: " + Math.round(dt), 300, 200);
+
         canvasArea.ctx.fillText("jumpValue: " + this.jumpValue, 300, 100);
         canvasArea.ctx.fillText("speed: " + this.speed, 300, 120);
 
 
         // STRAFE OPTIMIZER HUD
+        var strafeThreshold = 0.9 ** (0.08 * this.speed - 30);
 
-        if (Math.abs(touchHandler.dragAmount) < (0.9 ** (0.08 * this.speed - 30))) {
+        if (Math.abs(touchHandler.dragAmount) < (strafeThreshold * dt)) {
             ctx.fillStyle = "#FFFFFF";
         } else {
             ctx.fillStyle = "#FF0000";
         }
 
         ctx.fillRect(this.x-8, this.y + 16, 8, 4 * Math.abs(touchHandler.dragAmount)); // YOUR STRAFE
-        ctx.fillRect(this.x +4, this.y + 16, 8, 4 *  (0.9 ** (0.08 * this.speed - 30))); // THRESHOLD
+        ctx.fillRect(this.x +4, this.y + 16, 8, 4 * strafeThreshold * dt); // THRESHOLD
     
     }
+
 
     updatePos(dt) {  // NEEDS TO BE FPS INDEPENDENT
         this.angle += touchHandler.dragAmount * sensitivity; // gain calculation is independent of sensitivity BAD BAD BAD
 
         if (this.speed >= 0) { // PREVENTS GOING BACKWARDS
-            this.speed += this.calculateGain(touchHandler.dragAmount) * dt;
+            this.speed += this.calculateGain(touchHandler.dragAmount, dt);
         } else {this.speed = 0}
     
         this.x += Math.cos(this.angle * (Math.PI / 180)) * this.speed/50 * dt; // MOVE FORWARD AT ANGLE BASED ON SPEED
@@ -248,11 +214,11 @@ class Player {
         }
     }
 
-    calculateGain(drag) { // SHOULD HAVE A NICE CURVE WHERE LOWER SPEEDS REQUIRE MORE AGGRESSIVE STRAFES
+    calculateGain(drag, dt) { // COULD MAYBE PUT THIS INSIDE OF updatePos() to avoid having to pass dt
         
-        var strafeThreshold = 0.9 ** (0.08 * this.speed - 30);
+        var strafeThreshold = 0.9 ** (0.08 * this.speed - 30); // THROW IN DESMOS
 
-        if (Math.abs(drag) < strafeThreshold) {
+        if (Math.abs(drag) < strafeThreshold * dt) {
             return Math.abs(drag) * airAcceleration;
         } else {
             return -Math.abs(drag) * airAcceleration;
@@ -309,4 +275,4 @@ function updateGameArea() { // CALLED EVERY FRAME
 }
 
 
-//              :)
+//      :)
