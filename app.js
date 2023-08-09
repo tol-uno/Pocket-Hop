475b7f47a112037cdfe39c66e7d2e0fee5a012bd
toluno <unot46@gmail.com>
2023-08-09 00:54:13 -0400

    Added SAT collison WHOOP WHOOP

--- app.js
+++ app.js
@@ -79,6 +79,7 @@ var UserInterface = {
         canvasArea.ctx.fillText("angle: " + player.angle, textX, 140);
         canvasArea.ctx.fillText("timer: " + this.timer / 1000, textX, 160);
         canvasArea.ctx.fillText("renderedPlatforms Count: " + map.renderedPlatforms.length, textX, 180);
+        canvasArea.ctx.fillText("player x: " + player.x, textX, 200);
     }
 }
 
@@ -225,6 +226,8 @@ class Player {
         ctx.fillRect(-15, -15, 30, 30)
         ctx.filter = "none";
 
+
+
         ctx.rotate(-this.angle * Math.PI/180); // RE-rotating canvas
 
 
@@ -320,6 +323,8 @@ class Player {
         if (this.jumpValue < 0) { // JUMPING
             this.jumpValue = 0;
             this.jumpVelocity = 2;
+            console.log(this.checkCollision())
+             if (!this.checkCollision()) {this.restart()}
         } else {
             this.jumpValue += this.jumpVelocity * dt;
             this.jumpVelocity -= gravity * dt;
@@ -337,9 +342,217 @@ class Player {
         }
     }
 
-    checkCollision() {
-        this.renderedPlatforms.forEach(platform => { // LOOP THROUGH RENDERABLE PLATFORMS
+    checkCollision() { // called every time player hits the floor
+        var collision = 0;
+        map.renderedPlatforms.forEach(platform => { // LOOP THROUGH RENDERABLE PLATFORMS
+            
+
+            class Rectangle{
+                constructor(x,y,width,height,angle){
+                    this.x = x;
+                    this.y = y;
+                    this.width = width;
+                    this.height = height;
+                    this.angle = angle;
+                }
+            }
+
+            let rectangleStore = [
+                new Rectangle(player.x-16, player.y-16, 32, 32, player.angle),
+                new Rectangle(platform.x, platform.y, platform.width, platform.height, 0)
+            ]
+
+            canvasArea.ctx.fillRect(rectangleStore[0].x, rectangleStore[0].y, rectangleStore[0].width, rectangleStore[0].height)
+            console.log(rectangleStore[0])
+
+
+            function workOutNewPoints(cx, cy, vx, vy, rotatedAngle){ //From a rotated object
+                //cx,cy are the centre coordinates, vx,vy is the point to be measured against the center point
+                    //Convert rotated angle into radians
+                    rotatedAngle = rotatedAngle * Math.PI / 180;
+                    let dx = vx - cx;
+                    let dy = vy - cy;
+                    let distance = Math.sqrt(dx * dx + dy * dy);
+                    let originalAngle = Math.atan2(dy,dx);
+                    let rotatedX = cx + distance * Math.cos(originalAngle + rotatedAngle);
+                    let rotatedY = cy + distance * Math.sin(originalAngle + rotatedAngle);
+                
+                    return {
+                        x: rotatedX,
+                        y: rotatedY
+                    }
+            }
+            
+            //Get the rotated coordinates for the square
+            function getRotatedSquareCoordinates(square){
+                let centerX = square.x + (square.width / 2);
+                let centerY = square.y + (square.height / 2);
+                //Work out the new locations
+                let topLeft = workOutNewPoints(centerX, centerY, square.x, square.y, square.angle);
+                let topRight = workOutNewPoints(centerX, centerY, square.x + square.width, square.y, square.angle);
+                let bottomLeft = workOutNewPoints(centerX, centerY, square.x, square.y + square.height, square.angle);
+                let bottomRight = workOutNewPoints(centerX, centerY, square.x + square.width, square.y + square.height, square.angle);
+                return{
+                    tl: topLeft,
+                    tr: topRight,
+                    bl: bottomLeft,
+                    br: bottomRight
+                }
+            }
+            
+            //Functional objects for the Seperate Axis Theorum (SAT)
+            //Single vertex
+            function xy(x,y){
+                this.x = x;
+                this.y = y;
+            };
+            //The polygon that is formed from vertices and edges.
+            function polygon(vertices, edges){
+                this.vertex = vertices;
+                this.edge = edges;
+            };
+
+            //The actual Seperate Axis Theorum function
+            function sat(polygonA, polygonB){
+                var perpendicularLine = null;
+                var dot = 0;
+                var perpendicularStack = [];
+                var amin = null;
+                var amax = null;
+                var bmin = null;
+                var bmax = null;
+                //Work out all perpendicular vectors on each edge for polygonA
+                for(var i = 0; i < polygonA.edge.length; i++){
+                    perpendicularLine = new xy(-polygonA.edge[i].y,
+                                                polygonA.edge[i].x);
+                    perpendicularStack.push(perpendicularLine);
+                }
+                //Work out all perpendicular vectors on each edge for polygonB
+                for(var i = 0; i < polygonB.edge.length; i++){
+                    perpendicularLine = new xy(-polygonB.edge[i].y,
+                                                polygonB.edge[i].x);
+                    perpendicularStack.push(perpendicularLine);
+                }
+                //Loop through each perpendicular vector for both polygons
+                for(var i = 0; i < perpendicularStack.length; i++){
+                    //These dot products will return different values each time
+                    amin = null;
+                    amax = null;
+                    bmin = null;
+                    bmax = null;
+                    /*Work out all of the dot products for all of the vertices in PolygonA against the perpendicular vector
+                    that is currently being looped through*/
+                    for(var j = 0; j < polygonA.vertex.length; j++){
+                        dot = polygonA.vertex[j].x *
+                                perpendicularStack[i].x +
+                                polygonA.vertex[j].y *
+                                perpendicularStack[i].y;
+                        //Then find the dot products with the highest and lowest values from polygonA.
+                        if(amax === null || dot > amax){
+                            amax = dot;
+                        }
+                        if(amin === null || dot < amin){
+                            amin = dot;
+                        }
+                    }
+                    /*Work out all of the dot products for all of the vertices in PolygonB against the perpendicular vector
+                    that is currently being looped through*/
+                    for(var j = 0; j < polygonB.vertex.length; j++){
+                        dot = polygonB.vertex[j].x *
+                                perpendicularStack[i].x +
+                                polygonB.vertex[j].y *
+                                perpendicularStack[i].y;
+                        //Then find the dot products with the highest and lowest values from polygonB.
+                        if(bmax === null || dot > bmax){
+                            bmax = dot;
+                        }
+                        if(bmin === null || dot < bmin){
+                            bmin = dot;
+                        }
+                    }
+                    //If there is no gap between the dot products projection then we will continue onto evaluating the next perpendicular edge.
+                    if((amin < bmax && amin > bmin) ||
+                        (bmin < amax && bmin > amin)){
+                        continue;
+                    }
+                    //Otherwise, we know that there is no collision for definite.
+                    else {
+                        return false;
+                    }
+                }
+                /*If we have gotten this far. Where we have looped through all of the perpendicular edges and not a single one of there projections had
+                a gap in them. Then we know that the 2 polygons are colliding for definite then.*/
+                return true;
+            }
+
+            //Detect for a collision between the 2 rectangles
+            function detectRectangleCollision(index){
+                // let thisRect = rectangleStore[index];
+                // let otherRect = index === 0 ? rectangleStore[1] : rectangleStore[0];
+
+                let thisRect = rectangleStore[0];
+                let otherRect = rectangleStore[1];
+
+                //Get rotated coordinates for both rectangles
+                let tRR = getRotatedSquareCoordinates(thisRect);
+                let oRR = getRotatedSquareCoordinates(otherRect);
+                //Vertices & Edges are listed in clockwise order. Starting from the top right
+                let thisTankVertices = [
+                    new xy(tRR.tr.x, tRR.tr.y),
+                    new xy(tRR.br.x, tRR.br.y),
+                    new xy(tRR.bl.x, tRR.bl.y),
+                    new xy(tRR.tl.x, tRR.tl.y),
+                ];
+                let thisTankEdges = [
+                    new xy(tRR.br.x - tRR.tr.x, tRR.br.y - tRR.tr.y),
+                    new xy(tRR.bl.x - tRR.br.x, tRR.bl.y - tRR.br.y),
+                    new xy(tRR.tl.x - tRR.bl.x, tRR.tl.y - tRR.bl.y),
+                    new xy(tRR.tr.x - tRR.tl.x, tRR.tr.y - tRR.tl.y)
+                ];
+                let otherTankVertices = [
+                    new xy(oRR.tr.x, oRR.tr.y),
+                    new xy(oRR.br.x, oRR.br.y),
+                    new xy(oRR.bl.x, oRR.bl.y),
+                    new xy(oRR.tl.x, oRR.tl.y),
+                ];
+                let otherTankEdges = [
+                    new xy(oRR.br.x - oRR.tr.x, oRR.br.y - oRR.tr.y),
+                    new xy(oRR.bl.x - oRR.br.x, oRR.bl.y - oRR.br.y),
+                    new xy(oRR.tl.x - oRR.bl.x, oRR.tl.y - oRR.bl.y),
+                    new xy(oRR.tr.x - oRR.tl.x, oRR.tr.y - oRR.tl.y)
+                ];
+                let thisRectPolygon = new polygon(thisTankVertices, thisTankEdges);
+                let otherRectPolygon = new polygon(otherTankVertices, otherTankEdges);
+
+                if(sat(thisRectPolygon, otherRectPolygon)){
+                    collision += 1;
+                    
+                }else{
+                    
+                    // console.log("no collide")
+                    
+                    //Because we are working with vertices and edges. This algorithm does not cover the normal un-rotated rectangle
+                    //algorithm which just deals with sides
+                    if(thisRect.angle === 0 && otherRect.angle === 0){
+                        if(!(
+                            thisRect.x>otherRect.x+otherRect.width || 
+                            thisRect.x+thisRect.width<otherRect.x || 
+                            thisRect.y>otherRect.y+otherRect.height || 
+                            thisRect.y+thisRect.height<otherRect.y
+                        )){
+                            collision += 1;
+                        }
+                    }
+                }
+            }
+
+            detectRectangleCollision(platform);
+            
+
+
+        
         });
+        if (collision > 0) {return true} else {return false}
     }
 
     restart() {
--- test.js
+++ test.js
@@ -1,25 +1,274 @@
-function startGame() {
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+//Declare reference to btns
+const buttons = document.querySelectorAll("button");
 
-    const text = document.getElementById("text1");
-    let dt;
-
-    var lastUpdate = Date.now();
-    setInterval(tick, 1000/0);
-    setInterval(refreshCanvas, 1000/2);
-    
-    function tick() {
-        var now = Date.now();
-        dt = now - lastUpdate;
-        lastUpdate = now;
-    
-        gameLogic();
+class Rectangle{
+    constructor(x,y,width,height){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.moveDir = "NAN";
+        this.currRotation = 0;
+        this.rotationDir = "NAN";
+        this.color = "black";
     }
-
-    function refreshCanvas() { // Called at 60fps
-        text.innerHTML = (1000/dt);
+    move(){
+        if(this.moveDir === "LEFT"){
+            this.x -= 2;
+        }else if(this.moveDir === "RIGHT"){
+            this.x += 2;
+        }else if(this.moveDir === "DOWN"){
+            this.y += 2;
+        }else if(this.moveDir === "UP"){
+            this.y -= 2;
+        }
     }
-
-    function gameLogic() {
-        // console.log(1000 / dt);
+    rotation(){
+        if(this.rotationDir === "RIGHT"){
+            this.currRotation += 1;
+        }else if(this.rotationDir === "LEFT"){
+            this.currRotation -= 1;
+        }
+        ctx.translate(this.x + (this.width / 2), this.y + (this.height / 2));
+        ctx.rotate(this.currRotation * Math.PI / 180);
+        ctx.translate(-(this.x + (this.width / 2)), -(this.y + (this.height / 2)));
+        if(this.currRotation > 360 || this.currRotation < -360) this.currRotation = 0;
+    }
+    draw(){
+        this.move();
+        ctx.save();
+        this.rotation();
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.restore();
     }
 }
+
+let rectangleStore = [
+    new Rectangle(10, 200, 150, 150),
+    new Rectangle(440, 200, 150, 150)
+]
+
+function workOutNewPoints(cx, cy, vx, vy, rotatedAngle){ //From a rotated object
+    //cx,cy are the centre coordinates, vx,vy is the point to be measured against the center point
+        //Convert rotated angle into radians
+        rotatedAngle = rotatedAngle * Math.PI / 180;
+        let dx = vx - cx;
+        let dy = vy - cy;
+        let distance = Math.sqrt(dx * dx + dy * dy);
+        let originalAngle = Math.atan2(dy,dx);
+        let rotatedX = cx + distance * Math.cos(originalAngle + rotatedAngle);
+        let rotatedY = cy + distance * Math.sin(originalAngle + rotatedAngle);
+    
+        return {
+            x: rotatedX,
+            y: rotatedY
+        }
+}
+
+//Get the rotated coordinates for the square
+function getRotatedSquareCoordinates(square){
+    let centerX = square.x + (square.width / 2);
+    let centerY = square.y + (square.height / 2);
+    //Work out the new locations
+    let topLeft = workOutNewPoints(centerX, centerY, square.x, square.y, square.currRotation);
+    let topRight = workOutNewPoints(centerX, centerY, square.x + square.width, square.y, square.currRotation);
+    let bottomLeft = workOutNewPoints(centerX, centerY, square.x, square.y + square.height, square.currRotation);
+    let bottomRight = workOutNewPoints(centerX, centerY, square.x + square.width, square.y + square.height, square.currRotation);
+    return{
+        tl: topLeft,
+        tr: topRight,
+        bl: bottomLeft,
+        br: bottomRight
+    }
+}
+
+//Functional objects for the Seperate Axis Theorum (SAT)
+//Single vertex
+function xy(x,y){
+    this.x = x;
+    this.y = y;
+};
+//The polygon that is formed from vertices and edges.
+function polygon(vertices, edges){
+    this.vertex = vertices;
+    this.edge = edges;
+};
+
+//The actual Seperate Axis Theorum function
+function sat(polygonA, polygonB){
+    var perpendicularLine = null;
+    var dot = 0;
+    var perpendicularStack = [];
+    var amin = null;
+    var amax = null;
+    var bmin = null;
+    var bmax = null;
+    //Work out all perpendicular vectors on each edge for polygonA
+    for(var i = 0; i < polygonA.edge.length; i++){
+         perpendicularLine = new xy(-polygonA.edge[i].y,
+                                     polygonA.edge[i].x);
+         perpendicularStack.push(perpendicularLine);
+    }
+    //Work out all perpendicular vectors on each edge for polygonB
+    for(var i = 0; i < polygonB.edge.length; i++){
+         perpendicularLine = new xy(-polygonB.edge[i].y,
+                                     polygonB.edge[i].x);
+         perpendicularStack.push(perpendicularLine);
+    }
+    //Loop through each perpendicular vector for both polygons
+    for(var i = 0; i < perpendicularStack.length; i++){
+        //These dot products will return different values each time
+         amin = null;
+         amax = null;
+         bmin = null;
+         bmax = null;
+         /*Work out all of the dot products for all of the vertices in PolygonA against the perpendicular vector
+         that is currently being looped through*/
+         for(var j = 0; j < polygonA.vertex.length; j++){
+              dot = polygonA.vertex[j].x *
+                    perpendicularStack[i].x +
+                    polygonA.vertex[j].y *
+                    perpendicularStack[i].y;
+            //Then find the dot products with the highest and lowest values from polygonA.
+              if(amax === null || dot > amax){
+                   amax = dot;
+              }
+              if(amin === null || dot < amin){
+                   amin = dot;
+              }
+         }
+         /*Work out all of the dot products for all of the vertices in PolygonB against the perpendicular vector
+         that is currently being looped through*/
+         for(var j = 0; j < polygonB.vertex.length; j++){
+              dot = polygonB.vertex[j].x *
+                    perpendicularStack[i].x +
+                    polygonB.vertex[j].y *
+                    perpendicularStack[i].y;
+            //Then find the dot products with the highest and lowest values from polygonB.
+              if(bmax === null || dot > bmax){
+                   bmax = dot;
+              }
+              if(bmin === null || dot < bmin){
+                   bmin = dot;
+              }
+         }
+         //If there is no gap between the dot products projection then we will continue onto evaluating the next perpendicular edge.
+         if((amin < bmax && amin > bmin) ||
+            (bmin < amax && bmin > amin)){
+              continue;
+         }
+         //Otherwise, we know that there is no collision for definite.
+         else {
+              return false;
+         }
+    }
+    /*If we have gotten this far. Where we have looped through all of the perpendicular edges and not a single one of there projections had
+    a gap in them. Then we know that the 2 polygons are colliding for definite then.*/
+    return true;
+}
+
+//Detect for a collision between the 2 rectangles
+function detectRectangleCollision(index){
+    let thisRect = rectangleStore[index];
+    let otherRect = index === 0 ? rectangleStore[1] : rectangleStore[0];
+    //Get rotated coordinates for both rectangles
+    let tRR = getRotatedSquareCoordinates(thisRect);
+    let oRR = getRotatedSquareCoordinates(otherRect);
+    //Vertices & Edges are listed in clockwise order. Starting from the top right
+    let thisTankVertices = [
+        new xy(tRR.tr.x, tRR.tr.y),
+        new xy(tRR.br.x, tRR.br.y),
+        new xy(tRR.bl.x, tRR.bl.y),
+        new xy(tRR.tl.x, tRR.tl.y),
+    ];
+    let thisTankEdges = [
+        new xy(tRR.br.x - tRR.tr.x, tRR.br.y - tRR.tr.y),
+        new xy(tRR.bl.x - tRR.br.x, tRR.bl.y - tRR.br.y),
+        new xy(tRR.tl.x - tRR.bl.x, tRR.tl.y - tRR.bl.y),
+        new xy(tRR.tr.x - tRR.tl.x, tRR.tr.y - tRR.tl.y)
+    ];
+    let otherTankVertices = [
+        new xy(oRR.tr.x, oRR.tr.y),
+        new xy(oRR.br.x, oRR.br.y),
+        new xy(oRR.bl.x, oRR.bl.y),
+        new xy(oRR.tl.x, oRR.tl.y),
+    ];
+    let otherTankEdges = [
+        new xy(oRR.br.x - oRR.tr.x, oRR.br.y - oRR.tr.y),
+        new xy(oRR.bl.x - oRR.br.x, oRR.bl.y - oRR.br.y),
+        new xy(oRR.tl.x - oRR.bl.x, oRR.tl.y - oRR.bl.y),
+        new xy(oRR.tr.x - oRR.tl.x, oRR.tr.y - oRR.tl.y)
+    ];
+    let thisRectPolygon = new polygon(thisTankVertices, thisTankEdges);
+    let otherRectPolygon = new polygon(otherTankVertices, otherTankEdges);
+
+    if(sat(thisRectPolygon, otherRectPolygon)){
+        thisRect.color = "red";
+    }else{
+        thisRect.color = "black";
+        //Because we are working with vertices and edges. This algorithm does not cover the normal un-rotated rectangle
+        //algorithm which just deals with sides
+        if(thisRect.currRotation === 0 && otherRect.currRotation === 0){
+            if(!(
+                thisRect.x>otherRect.x+otherRect.width || 
+                thisRect.x+thisRect.width<otherRect.x || 
+                thisRect.y>otherRect.y+otherRect.height || 
+                thisRect.y+thisRect.height<otherRect.y
+            )){
+                thisRect.color = "red";
+            }
+        }
+    }
+}
+
+
+(function animate(){
+    requestAnimationFrame(animate);
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+
+    rectangleStore.forEach((rect, index) => {
+        detectRectangleCollision(index);
+        rect.draw();
+    });
+})();
+
+//Add event listener to all buttons
+buttons.forEach(button => {
+    button.addEventListener("mousedown", e => {
+        let btnType = e.target.getAttribute("data-btn-type");
+        let whichRect = null;
+        if(btnType.match(/1$/)){
+            whichRect = 0;
+        }else{
+            whichRect = 1;
+        }
+        if(btnType.match(/^left-btn/)){
+            rectangleStore[whichRect].moveDir = "LEFT";
+        }else if(btnType.match(/^right-btn/)){
+            rectangleStore[whichRect].moveDir = "RIGHT";
+        }else if(btnType.match(/^up-btn/)){
+            rectangleStore[whichRect].moveDir = "UP";
+        }else if(btnType.match(/^down-btn/)){
+            rectangleStore[whichRect].moveDir = "DOWN";
+        }else if(btnType.match(/^left-rotation/)){
+            rectangleStore[whichRect].rotationDir = "LEFT";
+        }else if(btnType.match(/^right-rotation/)){
+            rectangleStore[whichRect].rotationDir = "RIGHT";
+        }
+    });
+    addEventListener("mouseup", e => {
+        rectangleStore[0].moveDir = "NAN";
+        rectangleStore[0].rotationDir = "NAN";
+        rectangleStore[1].moveDir = "NAN";
+        rectangleStore[1].rotationDir = "NAN";
+    });
+    button.addEventListener("mouseout", e => {
+        rectangleStore[0].moveDir = "NAN";
+        rectangleStore[0].rotationDir = "NAN";
+        rectangleStore[1].moveDir = "NAN";
+        rectangleStore[1].rotationDir = "NAN";
+    });
+});
